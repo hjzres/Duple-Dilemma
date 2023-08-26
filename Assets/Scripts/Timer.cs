45224bc93932bb3e4908ee5a6c981f1f6974da4e
hjzre <142443853+hjzres@users.noreply.github.com>
2023-08-26 02:18:19 -0400

    #1 - Made a working countdown without something happening when the number hits 0

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Text timer;
+    [SerializeField] private float time;
+    private int t;
+    [SerializeField] private GameObject gm;
+    private GameManager gameManager;
+
+    private void Awake()
     {
-        
+        gameManager = gm.GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (t > 0)
+        {
+            time -= Time.deltaTime;
+            t = (int)time;
+            timer.text = t.ToString();
+        }
+        else
+        {
+
+        }
     }
 }
