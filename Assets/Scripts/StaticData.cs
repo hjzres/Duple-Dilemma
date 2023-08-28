17f19bbff8ec4601b25aa6600b05cca085a6aec1
hjzre <142443853+hjzres@users.noreply.github.com>
2023-08-28 18:41:59 -0400

    #11 - Added static data for all the stars in another script

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,11 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticData : MonoBehaviour
+{
+    public static int lvlOneStars;
+    public static int lvlTwoStars;
+    public static int lvlThreeStars;
+}
