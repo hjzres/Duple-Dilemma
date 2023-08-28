f7b84e7471b4ff028dc1b119276907d4f6b4683e
hjzre <142443853+hjzres@users.noreply.github.com>
2023-08-28 18:38:47 -0400

    #11 - stars can be filled in depending on the number of the stars variable

--- Assets/Scripts/UI/Stars.cs
+++ Assets/Scripts/UI/Stars.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
+    public int stars;
     [SerializeField] private GameObject[] filled;
     [SerializeField] private GameObject[] unfilled;
 
     private void Awake()
     {
+        stars = 0;
+
         foreach(GameObject fill in filled)
         {
             fill.SetActive(false);
         }
+    }
+
+    private void Update()
+    {
+        if(stars >= 1)
+        {
+            filled[0].SetActive(true);
+        }
+        if(stars >= 2)
+        {
+            filled[1].SetActive(true);
+        }
+        if(stars >= 3)
+        {
+            filled[2].SetActive(true);
+        }
+
     }
 }
