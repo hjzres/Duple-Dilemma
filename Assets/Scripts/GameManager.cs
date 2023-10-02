1922928c465dec52e95c5cc663ac82e194a520bd
hjzre <142443853+hjzres@users.noreply.github.com>
2023-10-02 19:44:41 -0400

    #5 - Level Done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,7 +29,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] int gameOverScene;
     [SerializeField] GameObject[] hearts;
 
-
     private void Awake()
     {
         playerOneSpawn = playerOne.transform.position;
