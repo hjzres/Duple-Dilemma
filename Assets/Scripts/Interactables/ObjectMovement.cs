d03ee0b5ca4ffd402b177afdecb72961e43e5dd1
Andrei <142443853+hjzres@users.noreply.github.com>
2023-10-30 18:44:17 -0400

    Finished the object movement

--- Assets/Scripts/Interactables/ObjectMovement.cs
+++ Assets/Scripts/Interactables/ObjectMovement.cs
@@ -1,34 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    private Rigidbody playerRigidbody;
-    private float pushSpeedMulplier = 0.75f;
-
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject playerOne, playerTwo;
+    public float pushingSpeed = 0.75f;
+    public bool isPushing = false;
+    private void OnCollisionEnter(Collision collision)
     {
-        playerRigidbody = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("obstacle"))
+        // this object will move when the player is touching it in the same direction the player is facing with the speed of the player's velocity multiplied by the push speed
+        if (collision.gameObject.tag == "player")
         {
-            Rigidbody boxRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            isPushing = true;
+            transform.position += collision.gameObject.GetComponent<Rigidbody>().velocity * pushingSpeed * Time.deltaTime;
+        }
 
-            Vector3 pushDirection = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z).normalized;
-            Vector3 pushForce = pushDirection * playerRigidbody.velocity.magnitude * pushSpeedMulplier;
+    }
 
-            boxRigidbody.AddForce(pushForce);
+    private void Update()
+    {
+        //When the player is holding e and the player is 5 units away from the object, the object will move with the player in its direction
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (Vector3.Distance(playerOne.transform.position, transform.position) < 5f)
+            {
+                // Calculate the direction vector from the object to the player
+                Vector3 direction = (playerOne.transform.position - transform.position).normalized;
+
+                // Move the object in the direction of the player
+                transform.position += direction * Time.deltaTime * pushingSpeed;
+            }
+            if (Vector3.Distance(playerTwo.transform.position, transform.position) < 5f)
+            {
+                // Calculate the direction vector from the object to the player
+                Vector3 direction = (playerTwo.transform.position - transform.position).normalized;
+
+                // Move the object in the direction of the player
+                transform.position += direction * Time.deltaTime * pushingSpeed;
+            }
         }
     }
+
 }
