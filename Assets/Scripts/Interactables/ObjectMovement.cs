4d58950d84ead1f3f2ac0130f843ae3a5cb76a71
Andrei <142443853+hjzres@users.noreply.github.com>
2023-10-30 17:03:23 -0400

    changed level 3

--- Assets/Scripts/Interactables/ObjectMovement.cs
+++ Assets/Scripts/Interactables/ObjectMovement.cs
@@ -31,4 +31,19 @@ public class ObjectMovement : MonoBehaviour
             boxRigidbody.AddForce(pushForce);
         }
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if the object with a tag "obstacle" enters the trigger and the player clicks the button "E" then the box will be pulled towards the player as he moves backwards
+        if (other.gameObject.CompareTag("obstacle") && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Test");
+            Rigidbody boxRigidbody = other.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 pullDirection = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z).normalized;
+            Vector3 pullForce = pullDirection * playerRigidbody.velocity.magnitude * pushSpeedMulplier;
+
+            boxRigidbody.AddForce(-pullForce);
+        }
+    }
 }
